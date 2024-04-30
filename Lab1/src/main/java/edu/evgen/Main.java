25f62ebffa2386347b265950e839f3972d741145
Evgeniy Fimushkin <fimnames@gmail.com>
2024-04-30 10:56:32 +0700

    JavaFX Init

--- Lab1/src/main/java/edu/evgen/Main.java
+++ Lab1/src/main/java/edu/evgen/Main.java
@@ -1,7 +1,26 @@
 package edu.evgen;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Worlds");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args){
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainScene.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        MainController controller = loader.getController();
+        controller.setStage(primaryStage);
+        primaryStage.setTitle("FXML Example");
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
---
+++ Lab1/src/main/java/edu/evgen/MainController.java
@@ -0,0 +1,18 @@
+package edu.evgen;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    private Stage stage;
+
+    @FXML
+    private void initialize(){
+
+    }
+
+    public void setStage(Stage primaryStage) {
+        stage = primaryStage;
+    }
+}
