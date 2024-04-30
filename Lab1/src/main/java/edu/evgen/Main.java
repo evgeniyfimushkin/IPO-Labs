7a4212463de45e185c7a3285c73a5ee68d83ed88
Evgeniy Fimushkin <fimnames@gmail.com>
2024-04-30 11:49:43 +0700

    selectable test table

--- Lab1/src/main/java/edu/evgen/Main.java
+++ Lab1/src/main/java/edu/evgen/Main.java
@@ -1,26 +1,68 @@
 package edu.evgen;
 
+import edu.evgen.Person;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.geometry.Orientation;
 
-public class Main extends Application {
-    public static void main(String[] args){
-        Application.launch();
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ChangeListener;
+
+public class Main extends Application{
+
+    public static void main(String[] args) {
+
+        Application.launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainScene.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        MainController controller = loader.getController();
-        controller.setStage(primaryStage);
-        primaryStage.setTitle("FXML Example");
-        primaryStage.setResizable(false);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+
+        // создаем список объектов
+        ObservableList<Person> people = FXCollections.observableArrayList(
+
+                new Person("Tom", 34),
+                new Person("Bob", 22),
+                new Person("Sam", 28),
+                new Person("Alice", 29)
+        );
+        Label lbl = new Label();
+        TableView<Person> table = new TableView<Person>(people);
+        table.setPrefWidth(250);
+        table.setPrefHeight(200);
+
+        // столбец для вывода имени
+        TableColumn<Person, String> nameColumn = new TableColumn<Person, String>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("name"));
+        table.getColumns().add(nameColumn);
+
+        // столбец для вывода возраста
+        TableColumn<Person, Integer> ageColumn = new TableColumn<Person, Integer>("Age");
+        ageColumn.setCellValueFactory(new PropertyValueFactory<Person, Integer>("age"));
+        table.getColumns().add(ageColumn);
+
+        TableView.TableViewSelectionModel<Person> selectionModel = table.getSelectionModel();
+        selectionModel.selectedItemProperty().addListener(new ChangeListener<Person>(){
+
+            public void changed(ObservableValue<? extends Person> val, Person oldVal, Person newVal){
+                if(newVal != null) lbl.setText("Selected: " + newVal.getName());
+            }
+        });
+
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, lbl, table);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        stage.setScene(scene);
+        stage.setTitle("TableView in JavaFX");
+        stage.show();
     }
 }
